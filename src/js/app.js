fb8a80d5cbe5d9b5233928e6ea03fd4307c1d42b
Oliver Inglis <ollie@theinglistwins.com>
2020-05-08 16:45:45 +0100

    trails

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,8 @@
 
 import { initThree } from './module/threee.js';
+import { initThree2 } from './module/three2.js';
 
-initThree();
+// initThree();
+import * as PointerLockControls from 'three-pointer-lock-controls';
+
+initThree2();
